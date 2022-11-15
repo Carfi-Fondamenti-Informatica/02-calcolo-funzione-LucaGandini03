b0f33942aa900cb5b6a44b1849cf85c88a9b7281
LucaGandini03 <116078651+LucaGandini03@users.noreply.github.com>
2022-11-15 14:21:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    float a = 0;
+    float b = 0;
+    float x = 0;
+    float y = 0;
+    float z = 0;
+    cin >> a >> b >> x >> y;
+    if (x < 0 < y) {
+        z = ((a * x) - (b * y));}
+    if (y <= 0 <= x) {
+        z = (a * (x * x)) - (b * y);}
+    else{
+       z=(a * x)+(b * (y * y));}
+    cout<<z;
    return 0;
 }
